4e87dad5a9033f05c149c67021eb06ffc62119ff
widua <mikolaj.widla@gmail.com>
2025-07-07 20:49:09 +0200

    First idea

--- src/main/java/command/WriteTreeCommand.java
+++ src/main/java/command/WriteTreeCommand.java
@@ -7,12 +7,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class WriteTreeCommand implements Command{
+public class WriteTreeCommand implements Command {
     private final GitParser parser;
-    List<String> ignored ;
+    List<String> ignored;
+    String currentDirectory = "";
+    Map<String, List<String>> structure = new HashMap<>();
 
     public WriteTreeCommand(GitParser parser) {
         this.parser = parser;
@@ -24,22 +25,39 @@ public class WriteTreeCommand implements Command{
         Path root = Paths.get("");
 
         resolveDirectory(root);
+        System.out.println(structure);
     }
 
 
-    private void resolveDirectory(Path path){
-        if (path.toString().contains(".git")|| ignored.stream().anyMatch((s)->path.toString().contains(s))  ){
+    private void resolveDirectory(Path path) {
+        if (path.toString().contains(".git") || ignored.stream().anyMatch((s) -> path.toString().contains(s.replace("/", "")))) {
             return;
         }
+        String currentPath = path.toString();
         File file = new File(path.toUri());
-        if (file.isDirectory()){
+        if (file.isDirectory()) {
+            String currentPathIn = currentPath.split("/")[currentPath.split("/").length - 1];
+            currentDirectory = currentPathIn;
+            List<String> fileList = new ArrayList<>();
+
+            fileList.add(currentDirectory+"/");
+            structure.put(currentPathIn,fileList);
             try {
                 Files.list(path).forEach(this::resolveDirectory);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         } else {
-            System.out.println(path);
+            String[] splittedPath = currentPath.split("/");
+            if (splittedPath.length > 1){
+                currentDirectory = splittedPath[splittedPath.length-2];
+            } else {
+                currentDirectory = "";
+            }
+            String currentFile = splittedPath[splittedPath.length-1];
+            List<String> currentFiles = structure.get(currentDirectory);
+            currentFiles.add(currentFile);
+            structure.put(currentDirectory, currentFiles);
         }
     }
 
